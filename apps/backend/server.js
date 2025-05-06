f51d0ffd456f669a564cc039ede4fff3ea42f09e
Daniel Esponda <daniel.esponda@datadoghq.com>
2025-05-06 13:06:00 +0000

    Update Dockerfile to expose port 5000 and refactor server.js entry point

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -1,6 +1,5 @@
-// filepath: /workspaces/pdf-checklists/server.js
 // This file serves as an entry point to our modularized server
-// It simply requires the actual server implementation
+const { startServer } = require("./server/server");
 
-// All the server logic has been moved to the server directory
-require("./server/server");
+// Start the server
+startServer();
